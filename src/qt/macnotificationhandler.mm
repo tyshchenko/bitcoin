004294e63f399c722f567ba4b90b2cdb2a979023
Jonas Schnelli <jonas.schnelli@include7.ch>
2013-04-09 14:25:14 +0200

    add notification support for mac 10.8 UserNotificationCenter
    - if the users runs on 10.8+, the new code now supports the UserNotificationCenter which is available from OSX 10.8 Mountain Lion (http://en.wikipedia.org/wiki/Notification_Center).
    - growl is still supported through a new implemented apple script bridge
    
    Signed-off-by: Jonas Schnelli <jonas.schnelli@include7.ch>

---
+++ src/qt/macnotificationhandler.mm
@@ -0,0 +1,62 @@
+#include "macnotificationhandler.h"
+
+#undef slots
+#include <Cocoa/Cocoa.h>
+
+void MacNotificationHandler::showNotification(const QString &title, const QString &text)
+{
+    // check if users OS has support for NSUserNotification
+    if(this->hasUserNotificationCenterSupport()) {
+        // okay, seams like 10.8+
+        QByteArray utf8 = title.toUtf8();
+        char* cString = (char *)utf8.constData();
+        NSString *titleMac = [[NSString alloc] initWithUTF8String:cString];
+
+        utf8 = text.toUtf8();
+        cString = (char *)utf8.constData();
+        NSString *textMac = [[NSString alloc] initWithUTF8String:cString];
+
+        NSUserNotification *userNotification = [[NSUserNotification alloc] init];
+
+        userNotification.title = titleMac;
+        userNotification.informativeText = textMac;
+
+        [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:userNotification];
+
+        [titleMac release];
+        [textMac release];
+        [userNotification release];
+    }
+}
+
+// sendAppleScript just take a QString and executes it as apple script
+void MacNotificationHandler::sendAppleScript(const QString &script) {
+    QByteArray utf8 = script.toUtf8();
+    char* cString = (char *)utf8.constData();
+    NSString *scriptApple = [[NSString alloc] initWithUTF8String:cString];
+
+    NSAppleScript *as = [[NSAppleScript alloc] initWithSource:scriptApple];
+    NSDictionary *err = nil;
+    [as executeAndReturnError:&err];
+    [as release];
+    [scriptApple release];
+}
+
+bool MacNotificationHandler::hasUserNotificationCenterSupport(void) {
+    Class possibleClass = NSClassFromString(@"NSUserNotificationCenter");
+
+    // check if users OS has support for NSUserNotification
+    if(possibleClass!=nil) {
+        return true;
+    }
+    return false;
+}
+
+
+MacNotificationHandler *MacNotificationHandler::instance()
+{
+    static MacNotificationHandler *s_instance = NULL;
+    if (!s_instance)
+        s_instance = new MacNotificationHandler();
+    return s_instance;
+}
