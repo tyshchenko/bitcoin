cde0fb520cc22bf2b60acade7f4edf7f46e4cced
Jonas Schnelli <dev@jonasschnelli.ch>
2016-12-30 13:55:22 +0100

    Keep track of the headers chain tip to detect forks

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -664,6 +664,7 @@ public:
         nLastResend = 0;
         nTimeFirstKey = 0;
         fBroadcastTransactions = false;
+        pLastKnownBestHeader = NULL;
     }
 
     std::map<uint256, CWalletTx> mapWallet;
@@ -683,6 +684,9 @@ public:
     std::set<COutPoint> setLockedCoins;
 
     int64_t nTimeFirstKey;
+
+    //! last known best header, required to check for forks
+    CBlockIndex *pLastKnownBestHeader;
 
     const CWalletTx* GetWalletTx(const uint256& hash) const;
 
