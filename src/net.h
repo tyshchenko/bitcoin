280c67f2596d35694a0cf4a558a1b8279e1d7fa2
Jonas Schnelli <dev@jonasschnelli.ch>
2018-08-06 16:22:20 +0200

    Use unique pointers for CNetMessage handling

--- src/net.h
+++ src/net.h
@@ -621,6 +621,7 @@ public:
     int readData(const char *pch, unsigned int nBytes);
 };
 
+typedef std::unique_ptr<CNetMessage> CNetMessageRef;
 
 /** Information about a peer */
 class CNode
@@ -639,7 +640,7 @@ public:
     CCriticalSection cs_vRecv;
 
     CCriticalSection cs_vProcessMsg;
-    std::list<CNetMessage> vProcessMsg;
+    std::list<CNetMessageRef> vProcessMsg;
     size_t nProcessQueueSize;
 
     CCriticalSection cs_sendProcessing;
@@ -758,7 +759,7 @@ private:
     const ServiceFlags nLocalServices;
     const int nMyStartingHeight;
     int nSendVersion;
-    std::list<CNetMessage> vRecvMsg;  // Used only by SocketHandler thread
+    std::list<CNetMessageRef> vRecvMsg; // Used only by SocketHandler thread
 
     mutable CCriticalSection cs_addrName;
     std::string addrName;
