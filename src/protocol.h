3a68627fe73cf7d078785fad6f4ed11cbcd36b6c
Jonas Schnelli <dev@jonasschnelli.ch>
2018-08-15 09:13:27 +0200

    Add and respect service flag NODE_ENCRYPTED

--- src/protocol.h
+++ src/protocol.h
@@ -268,6 +268,9 @@ enum ServiceFlags : uint64_t {
     // serving the last 288 (2 day) blocks
     // See BIP159 for details on how this is implemented.
     NODE_NETWORK_LIMITED = (1 << 10),
+    // NODE_ENCRYPTED means that the node does accept encrypted communication after BIP151
+    // See BIP151 for details on how this is implemented.
+    NODE_ENCRYPTED = (1 << 11),
 
     // Bits 24-31 are reserved for temporary experiments. Just pick a bit that
     // isn't getting used, or one not being used much, and notify the
