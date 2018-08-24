4e1d86be0d2f0b8ff7a6627398b9896c2e0af1ea
Jonas Schnelli <dev@jonasschnelli.ch>
2018-08-24 16:54:07 +0200

    Add chacha20/poly1305 and chacha20poly1305_AEAD from openssh

---
+++ src/crypto/chachapoly_aead.h
@@ -0,0 +1,22 @@
+#ifndef BITCOIN_CRYPTO_CHACHAPOLY_AEAD_H
+#define BITCOIN_CRYPTO_CHACHAPOLY_AEAD_H
+
+#include <crypto/chacha.h>
+
+#define CHACHA_KEYLEN 32 /* 2 x 256 bit keys */
+
+struct chachapolyaead_ctx {
+  struct chacha_ctx main_ctx, header_ctx;
+};
+
+int chacha20poly1305_init(struct chachapolyaead_ctx *cpctx, const uint8_t *key,
+                          int keylen);
+int chacha20poly1305_crypt(struct chachapolyaead_ctx *ctx, uint32_t seqnr,
+                           uint8_t *dest, const uint8_t *src, uint32_t len,
+                           uint32_t aadlen, int is_encrypt);
+
+// extracts the LE 24bit (3byte) length from the AAD and puts it into a uint32_t (host endianness)
+int chacha20poly1305_get_length24(struct chachapolyaead_ctx *ctx,
+                                uint32_t *len_out, uint32_t seqnr,
+                                const uint8_t *ciphertext);
+#endif /* BITCOIN_CRYPTO_CHACHAPOLY_AEAD_H */
